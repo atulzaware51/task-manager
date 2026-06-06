142bfe028052d1ff4e4c0d7abf91be1124106cbe
atulzaware51 <atulzaware1411@gmail.com>
2026-06-06 10:16:19 +0000

    Added animated todo list UI

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,56 @@
 function addTask(){
 
-let task=document.getElementById("task").value;
+    let input=document.getElementById("taskInput");
 
-let li=document.createElement("li");
+    let task=input.value.trim();
 
-li.innerText=task;
+    if(task===""){
+        alert("Please enter a task");
+        return;
+    }
 
-document.getElementById("taskList")
-.appendChild(li);
+    let li=document.createElement("li");
 
+    li.innerHTML=`
+        <span>${task}</span>
+
+        <div class="task-buttons">
+
+            <button class="complete-btn"
+            onclick="toggleTask(this)">
+            ✓
+            </button>
+
+            <button class="delete-btn"
+            onclick="deleteTask(this)">
+            ✕
+            </button>
+
+        </div>
+    `;
+
+    document.getElementById("taskList")
+    .appendChild(li);
+
+    input.value="";
+}
+
+function toggleTask(button){
+
+    let taskText=
+    button.parentElement.previousElementSibling;
+
+    taskText.classList.toggle("completed");
+}
+
+function deleteTask(button){
+
+    let li=button.parentElement.parentElement;
+
+    li.style.transform="translateX(100px)";
+    li.style.opacity="0";
+
+    setTimeout(()=>{
+        li.remove();
+    },300);
 }
