e149145e5a2cf355979f52d8f3476f6782774995
atulzaware51 <atulzaware1411@gmail.com>
2026-06-06 10:07:42 +0000

    Added CSS styling

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+function addTask(){
+
+let task=document.getElementById("task").value;
+
+let li=document.createElement("li");
+
+li.innerText=task;
+
+document.getElementById("taskList")
+.appendChild(li);
+
+}
